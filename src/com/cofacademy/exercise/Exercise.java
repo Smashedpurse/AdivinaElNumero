ea98ef0ef85fa585a3f2f9773bea587856e0d395
Ivan Rubio <smashedpurse@gmail.com>
2022-12-23 17:13:05 -0600

    Configuration of the game

--- src/com/cofacademy/exercise/Exercise.java
+++ src/com/cofacademy/exercise/Exercise.java
@@ -5,21 +5,24 @@ import javax.swing.*;
 public class Exercise {
     public static void main(String[] args) {
 
-        String math = JOptionPane.showInputDialog("Insert the math calification");
+        String math = JOptionPane.showInputDialog("Insert the Math calification");
         float mathScore = Float.parseFloat(math);
-        String spanish = JOptionPane.showInputDialog("Insert the spanish calification");
+        String spanish = JOptionPane.showInputDialog("Insert the Spanish calification");
         float spanishScore = Float.parseFloat(spanish);
-        String french = JOptionPane.showInputDialog("Insert the spanish calification");
+        String french = JOptionPane.showInputDialog("Insert the French calification");
         float frenchScore = Float.parseFloat(french);
-        String History = JOptionPane.showInputDialog("Insert the spanish calification");
+        String History = JOptionPane.showInputDialog("Insert the History calification");
         float HistoryScore = Float.parseFloat(History);
+        String Chemistry = JOptionPane.showInputDialog("Insert the Chemistry calification");
+        float ChemistryScore = Float.parseFloat(Chemistry);
 
-        float schoolGrades [] = {mathScore + spanishScore + frenchScore + HistoryScore};
-        float schoolSubjects [] = {mathScore , spanishScore , frenchScore, HistoryScore };
+
+        float schoolGrades [] = {mathScore + spanishScore + frenchScore + HistoryScore + ChemistryScore};
+        float schoolSubjects [] = {mathScore , spanishScore , frenchScore, HistoryScore,ChemistryScore };
 
         for (float result: schoolGrades
              ) {
-            System.out.println(result / schoolSubjects.length);
+            JOptionPane.showMessageDialog(null,"The result of your school Grades is  " + result / schoolSubjects.length);
         }
     }
 }
